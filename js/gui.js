0f704ff7caa5bcee281199c4b442b870e07abda4
Nik <bohrnks@gmail.com>
2025-03-27 11:38:37 -0700

    move to top

--- js/gui.js
+++ js/gui.js
@@ -1,5 +1,22 @@
 const gui = new dat.GUI({ width: 400, closed: true })
 
+// Load volume from localStorage or use default
+const savedVolume = Number(localStorage.getItem('engineVolume'));
+const defaultVolume = 50;
+
+// Add volume control at the top level
+const volumeControl = {
+  volume: !isNaN(savedVolume) ? savedVolume : defaultVolume
+}
+
+// Add volume slider (0-100%) at the top
+gui.add(volumeControl, 'volume', 0, 100).step(1)
+  .name('Engine Volume %')
+  .onChange((value) => {
+    vehicleParams.volume = value;
+    localStorage.setItem('engineVolume', value);
+  });
+
 // Input controls folder at the top
 const inputControls = {
   steering: 0,    // -1 to 1
@@ -31,10 +48,6 @@ gui.add(params, 'pushForce', -10, 10)
 gui.add(params, 'sideForceMultiplier', -2000, 2000)
 gui.add(params, 'asdf', -4000, 4000).listen()
 
-// Load volume from localStorage or use default
-const savedVolume = Number(localStorage.getItem('engineVolume'));
-const defaultVolume = 50;
-
 const vehicleParams = {
   speed: 0, // Will be updated from code
   slipRatio: 0,
@@ -49,12 +62,7 @@ const vehicleParams = {
   forceDirY: 0,
   forceDirZ: 0,
   forwardForceScalar: 0,
-  volume: !isNaN(savedVolume) ? savedVolume : defaultVolume // Use saved volume if valid, otherwise default
-}
-
-// Save volume to localStorage when it changes
-const saveVolumeToStorage = (value) => {
-  localStorage.setItem('engineVolume', value);
+  volume: volumeControl.volume // Initialize with the volume control value
 }
 
 const vehicleData = gui.addFolder('Vehicle data')
@@ -79,9 +87,4 @@ forceDirYController.listen()
 const forceDirZController = forceFolder.add(vehicleParams, 'forceDirZ', -100, 100).step(0.1)
 forceDirZController.listen()
 
-forceFolder.open()
-
-// Add volume slider (0-100%)
-vehicleData.add(vehicleParams, 'volume', 0, 100).step(1)
-  .name('Engine Volume %')
-  .onChange(saveVolumeToStorage) 
+forceFolder.open() 
