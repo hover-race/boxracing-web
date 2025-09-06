da3f6d4328ddd63d65f82eb27e532bc29a6e50da
Nik <bohrnks@gmail.com>
2025-09-06 15:12:54 -0700

    works from any dir

--- chrome-bridge/read-console.sh
+++ chrome-bridge/read-console.sh
@@ -2,4 +2,9 @@
 # Chrome Remote Debugger Console Reader
 # Pass through all options to check-console.js
 
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Run the node script from the script's directory
+cd "$SCRIPT_DIR"
 node check-console.js "$@"
