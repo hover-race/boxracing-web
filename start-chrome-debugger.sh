06be904e7c1a4a335a44034fff86bd484a5516b6
Nik <bohrnks@gmail.com>
2025-09-06 15:06:14 -0700

    bg options

--- start-chrome-debugger.sh
+++ start-chrome-debugger.sh
@@ -1,3 +1,7 @@
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
   --remote-debugging-port=9222 \
-  --user-data-dir=/tmp/chrome-debug
+  --user-data-dir=/tmp/chrome-debug \
+  --disable-background-timer-throttling \
+  --disable-renderer-backgrounding \
+  --disable-backgrounding-occluded-windows \
+  --disable-ipc-flooding-protection
